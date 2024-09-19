b04eb01ca01e614188deae3d1821b8e73e1f62b5
bradflorea <bradleyaflorea@gmail.com>
2024-09-19 18:03:54 -0400

    added gitignore

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/hello")
-def home():
-    return "Hello, World!"
+@app.route("/")
+def say_hello():
+    html = """
+    <html>
+     <body>
+     <h1>Hello, World!</h1>
+      <p>This is a simple Flask application.</p>
+     </body>
+    </html>
+    """
+    return html
+
+
+@app.route("/goodbye")
+def say_goodbye():
+    return "Goodbye, World!"

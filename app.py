7665d99ad85098c2a679ca3cced635afbdd382e2
bradflorea <bradleyaflorea@gmail.com>
2024-09-30 18:49:22 -0400

    POST REQUESTS

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def say_hello():
     <html>
      <body>
      <h1>Hello, World!</h1>
-      <p>This is a simple Flask application.</p>
+      <p>This is the hello page.</p>
      </body>
     </html>
     """
@@ -19,3 +19,38 @@ def say_hello():
 @app.route("/goodbye")
 def say_goodbye():
     return "Goodbye, World!"
+
+
+@app.route("/search")
+def search():
+    term = request.args["term"]
+    sort = request.args["sort"]
+    return f"<h1>You searched for:  {term}</h1> <p>Sorting by : {sort}</p>"
+
+
+# @app.route("/post", methods=["POST"])
+# def post_demo():
+#     return "This is a POST request"
+
+
+# @app.route("/post", methods=["GET"])
+# def post_demo():
+#     return "This is a GET request"
+
+
+@app.route("/add-comment")
+def add_comment_form():
+    return """
+    <h1>Add Comment Form</h1>
+    <form method="POST">
+        <input type="text" name="comment" placeholder="Enter your comment">
+        <button>Submit</button>
+        </form>
+    """
+
+
+@app.route("/add-comment", methods=["POST"])
+def save_comment():
+    return """
+    <h1>Comment saved!</h1>
+    """
